5d70db8a38fae2b0fe4786eb2350de0f6385488f
cecil <1260750674@qq.com>
2024-10-13 17:21:47 +0800

    Site updated: 2024-10-13 17:21:39

--- sw.js
+++ sw.js
@@ -49,7 +49,7 @@
     self.addEventListener('activate', event => event.waitUntil(clients.claim()))
 
     // noinspection JSFileReferences
-    let skipRequest = request => request.url.startsWith('https://i0.hdslb.com')
+    let skipRequest = request => request.url.startsWith('https://i0.hdslb.com') || request.url.startsWith('https://music.163.com')
 let cacheRules = {
 simple: {
 clean: true,
