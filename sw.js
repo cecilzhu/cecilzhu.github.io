470b9789dd53568e55c8f17dcef8d706d05ec105
cecil <1260750674@qq.com>
2024-10-18 06:44:28 +0800

    Site updated: 2024-10-18 06:44:21

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@
 
     self.addEventListener('install', () => {
         self.skipWaiting()
-        const escape = 1
+        const escape = 0
         if (escape) {
             readVersion().then(async oldVersion => {
                 // noinspection JSIncompatibleTypesComparison
@@ -319,7 +319,7 @@ const fetchFile = (request, banCache, urls = null) => {
             // 新用户和刚进行过逃逸操作的用户不进行更新操作
             if (!oldVersion) {
                 // noinspection JSValidateTypes
-                newVersion.escape = 1
+                newVersion.escape = 0
                 writeVersion(newVersion)
                 return {new: newVersion, old: oldVersion}
             }
